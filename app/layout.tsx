461302e285baa8ad9dc11c4b1740899634f5b500
Chris Pyle <cpyle@unicode.org>
2025-06-17 08:37:39 -0400

    button component

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,8 +36,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`min-h-screen bg-white`}>
+    <html lang="en" className="dark">
+      <body className="min-h-screen">
         <Header />
         <main className="relative z-10 pt-16">{children}</main>
       </body>
